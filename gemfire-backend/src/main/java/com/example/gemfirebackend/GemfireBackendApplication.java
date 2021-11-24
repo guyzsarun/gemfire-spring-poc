4b18ad02cb2df1b5002681dec1420e215eda5e5a
guyzsarun <sarun.nunt@gmail.com>
2021-11-24 15:56:31 +0700

    init

---
+++ gemfire-backend/src/main/java/com/example/gemfirebackend/GemfireBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gemfirebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GemfireBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GemfireBackendApplication.class, args);
+	}
+
+}
---
+++ gemfire-backend/src/test/java/com/example/gemfirebackend/GemfireBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gemfirebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GemfireBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
