efa7d11a37729ecc25b02ea2660f32f14372a385
guyzsarun <sarun.nunt@gmail.com>
2021-11-30 12:00:23 +0700

    Update docs

--- gemfire-backend/src/main/java/com/example/gemfirebackend/movie/MovieController.java
+++ gemfire-backend/src/main/java/com/example/gemfirebackend/movie/MovieController.java
@@ -2,27 +2,20 @@ package com.example.gemfirebackend.movie;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URISyntaxException;
 import java.util.Map;
 
-//@Operation(summary = "Get movie by title")
 @RestController
 @RequestMapping(path = "api/movie")
 @OpenAPIDefinition
@@ -70,7 +63,5 @@ public class MovieController {
         }
         response.put("movie",movieList);
         return response.toMap();
-
-
     }
 }
