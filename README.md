eb10a7d95ebd33bf2e3aef71b301e650ece657d8
Sarun Nuntaviriyakul <48861591+guyzsarun@users.noreply.github.com>
2021-12-03 13:32:15 +0700

    Chore: Docs (#3)
    * Chore: Add documentation
    
    * Update docs
    
    * Add dockerfile
    
    * Add dockerignore
    
    * Add cluster configuration

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# gemfire-spring-poc
+# TanzuGemfire-SpringBoot-POC
+
+## Installation
+
+Clone the repo from Github
+
+```
+git clone  https://github.com/guyzsarun/gemfire-spring-poc.git
+```
+
+Update api secret in `application.properties` ( Refer to `example.application.properties` )<br>
+Movie API from [RapidAPI](https://rapidapi.com/apidojo/api/imdb8/)
+
+```
+api.rapid.host=
+api.rapid.key=
+```
+
+## API
+
+SpringBoot backend available at http://localhost:8081/api/movie <br>
+Swagger documentation available at http://localhost:8081/docs.html
+
+### Example Response
+
+#### Request
+
+`GET /api/movie/`
+
+```sh
+curl --location --request GET 'localhost:8081/api/movie?name=avatar'
+```
+
+#### Response
+
+```yaml
+{
+  "movie":
+    [
+      {
+        "title": "Avatar: The Last Airbender",
+        "year": "2005",
+        "rank": "419",
+        "id": "tt0417299",
+        "poster": "https://m.media-amazon.com/images/M/MV5BODc5YTBhMTItMjhkNi00ZTIxLWI0YjAtNTZmOTY0YjRlZGQ0XkEyXkFqcGdeQXVyODUwNjEzMzg@._V1_.jpg",
+      },
+    ],
+  "delay(ms)": 10,
+}
+```
+
+## Project Structure
+
+    .
+    ├── gemfire-backend               # SpringBoot Application
+    ├── gemfire-cluster               # Gemfire Cluster on Kubernetes
+    │
+    └── README.md
+
+## Usage
+
+Create storage region
+
+```sh
+create region --name=movie --type=REPLICATE_HEAP_LRU --entry-idle-time-expiration=3600 --enable-statistics
+```
+
+## Notes
--- gemfire-backend/src/main/java/com/example/gemfirebackend/movie/MovieController.java
+++ gemfire-backend/src/main/java/com/example/gemfirebackend/movie/MovieController.java
@@ -1,23 +1,24 @@
 package com.example.gemfirebackend.movie;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URISyntaxException;
 import java.util.Map;
 
 @RestController
 @RequestMapping(path = "api/movie")
+@OpenAPIDefinition
 public class MovieController {
     Logger logger = LoggerFactory.getLogger(MovieController.class);
 
@@ -25,6 +26,10 @@ public class MovieController {
     MovieService movieService;
 
     @GetMapping
+    @Operation(summary = "Get Movies", description = "Get movies by title", responses={
+            @ApiResponse(description = "Success", responseCode = "200", content = @Content(mediaType = "application/json")),
+            @ApiResponse(description = "Internal error", responseCode = "500", content = @Content)
+    })
     public Map<String, Object> getMovie(@RequestParam(value = "name", required = false , defaultValue = "") String name)
     {
         long start = System.currentTimeMillis();
@@ -58,7 +63,5 @@ public class MovieController {
         }
         response.put("movie",movieList);
         return response.toMap();
-
-
     }
 }
